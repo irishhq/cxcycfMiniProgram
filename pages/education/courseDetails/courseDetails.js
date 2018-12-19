d704c51dac60d65574ad300339fc96129a042f6f
irishhq <iris_hhqian@163.com>
2018-12-19 21:13:45 +0800

    添加加’添加课程状态

--- pages/education/courseDetails/courseDetails.js
+++ pages/education/courseDetails/courseDetails.js
@@ -21,7 +21,9 @@ Page({
     itemSelected: [
       { isSelected: true, show: true },
       { isSelected: false, show: false }
-    ]
+    ],
+    /*学习状态*/
+    courseLearning: ""
   },
   inputValue: '',
   bindSendDanmu: function () {
@@ -50,12 +52,42 @@ Page({
       });
     }
   },
+  joinLearning: function () {
+    try {
+      const status = wx.getStorageSync('key');      /*获取课程学习状态*/
+      var msg = "";
+      if (0 == status) {                            /*该课程未被加入学习*/
+        msg = "加入学习成功"
+        this.setData({                                /*修改'加入学习'为继续学习*/
+          courseLearning: "继续学习"
+        });
+      } else {
 
+      }
+      wx.showToast({                                /*显示加入学习成功提示框*/
+        title: msg,
+        icon: "success",
+        duration: 2000
+      });
+
+    } catch (e) {
+      throw e;
+    }
+    /*将课程学习状态保存*/
+    wx.setStorage({
+      key: ""
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    /*加载页面时，获取课程学习状态：显示加入学习/继续学习*/
+    
+    var courLearingStatus = "加入学习";
+    this.setData({
+      courseLearning: courLearingStatus
+    })
   },
 
   /**
